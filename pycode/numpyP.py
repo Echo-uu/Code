9a96cf99f8a46e1a883a16f4b8e18932d2243614
Echo <1741399839@qq.com>
2020-09-18 18:16:20 +0800

    numpy code

--- pycode/numpyP.py
+++ pycode/numpyP.py
@@ -35,10 +35,27 @@ def costTime():
     return
 
 def ndarrayTest():
-    data = np.random.randn(2, 3)
+    data = np.random.randn(6, 3)
     print(data, data + data, data * 10)
     print('data shape', data.shape, '\n', 'data dtype', data.dtype)
     print('data ndim', data.ndim)
+    #
+    numericStrings = np.array(['1.456', '2.545', '454.55'], dtype = np.string_)
+    print(numericStrings)
+    print(numericStrings.astype(float))
+    #slice修改原数组
+    arr = np.arange(10)
+    print(arr)
+    arrSlice = arr[3:8]
+    arrSlice -= 1
+    print(arr)
+    #
+    arr1 = np.arange(32).reshape(8,4)
+    print(arr1)
+    print(arr1[[1, 5, 7, 2]][:,[0, 3, 1, 2]])
+    #矩阵转置，内积
+    print(data.T)
+    print(np.dot(data.T,data))
     return
 
 ndarrayTest()
